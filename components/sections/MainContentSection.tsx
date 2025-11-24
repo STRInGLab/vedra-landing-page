55ba1537506bdf1fdd4e88214b40258c3b3a05d2
Sujan R V <sujanrv99@Gmail.com>
2025-11-24 13:19:48 +0530

    shadow gradient fix

--- components/sections/MainContentSection.tsx
+++ components/sections/MainContentSection.tsx
@@ -1,6 +1,5 @@
-import { CheckIcon, PlayIcon } from "lucide-react";
-import React from "react";
 import { Button } from "@/components/ui/button";
+import { CheckIcon, PlayIcon } from "lucide-react";
 
 const features = [
   { text: "Free 14-Day trial" },
@@ -10,8 +9,8 @@ const features = [
 
 export const MainContentSection = (): JSX.Element => {
   return (
-    <section className="relative flex flex-col w-full items-center pt-24 md:pt-32 lg:pt-48 xl:pt-60 pb-[200px] sm:pb-[300px] md:pb-[350px] lg:pb-[450px] px-4 sm:px-6 md:px-12 lg:px-16 bg-[linear-gradient(205deg,rgba(0,0,0,1)_0%,rgba(36,72,128,1)_20%,rgba(0,27,102,1)_30%,rgba(0,0,0,1)_40%,rgba(0,0,0,1)_50%,rgba(0,0,0,1)_60%,rgba(0,27,102,1)_70%,rgba(36,72,128,1)_80%,rgba(0,0,0,1)_100%)]">
-      <div className="absolute bottom-0 left-0 right-0 h-20 bg-gradient-to-b from-transparent to-white pointer-events-none" style={{ backdropFilter: 'blur(8px)' }}></div>
+    <section className="relative z-0 flex flex-col w-full items-center pt-24 md:pt-32 lg:pt-48 xl:pt-60 pb-[200px] sm:pb-[300px] md:pb-[350px] lg:pb-[450px] px-4 sm:px-6 md:px-12 lg:px-16 bg-[linear-gradient(205deg,rgba(0,0,0,1)_0%,rgba(36,72,128,1)_20%,rgba(0,27,102,1)_30%,rgba(0,0,0,1)_40%,rgba(0,0,0,1)_50%,rgba(0,0,0,1)_60%,rgba(0,27,102,1)_70%,rgba(36,72,128,1)_80%,rgba(0,0,0,1)_100%)] [mask-image:linear-gradient(to_bottom,black_calc(100%-75px),transparent_100%)]">
+      <div className="absolute -bottom-2 left-0 right-0 h-[110px] bg-gradient-to-t from-white via-white/70 to-transparent z-10 blur-xxl"></div>
       <div className="flex flex-col gap-6 sm:gap-8 md:gap-10 w-full max-w-[1200px] items-center justify-center relative z-10">
         <div className="inline-flex flex-col items-center gap-3 sm:gap-4 px-4">
           <h1 className="flex flex-wrap items-center justify-center max-w-full md:max-w-[964px] [font-family:'Instrument_Sans',Helvetica] font-bold text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl text-center tracking-tight md:tracking-[-2.16px] leading-tight sm:leading-[1.2] md:leading-[80px]">
